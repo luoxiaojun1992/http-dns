f7a9fc1b4f34f5572f2dbcd0cf2944accb54db4b
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-21 17:56:47 +0800

    add model

--- models/ip_list.go
+++ models/ip_list.go
@@ -7,7 +7,7 @@ type IpList struct {
 	Region string `xorm:"varchar(255) notnull default '' 'region'"`
 	ServiceName string `xorm:"varchar(255) notnull default '' 'service name'"`
 	Ip string `xorm:"varchar(20) notnull default '' 'ip'"`
-	Ttl string `xorm:""`
-	CreatedAt time.Time `xorm:"TIMESTAMP notnull 'created at'"`
-	UpdatedAt time.Time `xorm:"TIMESTAMP notnull 'updated at'"`
+	Ttl string `xorm:"varchar(255) not null default '0' 'ttl'"`
+	CreatedAt time.Time `xorm:"TIMESTAMP notnull created 'created at'"`
+	UpdatedAt time.Time `xorm:"TIMESTAMP notnull updated 'updated at'"`
 }
