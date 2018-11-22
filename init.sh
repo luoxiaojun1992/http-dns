af41cea9c9ce6fda189e870f12ebba0d0d1b4e4c
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-22 19:21:33 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-if [ ! -e ./vendor ]; then
-	$GOPATH/bin/govendor init
-	$GOPATH/bin/govendor fetch github.com/gin-gonic/gin@v1.3
-	curl https://raw.githubusercontent.com/gin-gonic/gin/master/examples/basic/main.go > main.go
+if [ ! -e ./vendor/github.com ]; then
+	$GOPATH/bin/govendor sync
+fi
+
+if [ ! -e ./.env ]; then
+    mv ./.env.example ./.env
 fi
