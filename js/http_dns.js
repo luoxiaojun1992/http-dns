6a405ec09af080d2a9c430bd40bb47707d9861c9
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-29 18:45:46 +0800

    update js

--- js/http_dns.js
+++ js/http_dns.js
@@ -1,7 +1,35 @@
-fetch('http://localhost:52273/ips?region=sh&service-name=user-service', {"METHOD":"GET"}).
-then(function(response){
-    response.json().
-    then(function(data) {
-        localStorage.setItem('dns:sh:user-service', data)
+function updateDnsCache(region, serviceName) {
+    fetch('http://localhost:57267/ips?region=' + region + '&service-name=' + serviceName, {"METHOD": "GET"}).then(function (response) {
+        response.json().then(function (data) {
+            if (data.code === 0) {
+                let ips = data.data.ips;
+                ips.forEach(function (ip, i) {
+                    ips[i].ttl = parseInt(Date.parse(new Date()) / 1000) + parseInt(ip.ttl)
+                });
+
+                localStorage.setItem('dns:' + region + ':' + serviceName, JSON.stringify(ips));
+            }
+        });
     });
-});
+}
+
+function resolveName(region, serviceName, once = false) {
+    const ipsStr = localStorage.getItem('dns:' + region + ':' + serviceName);
+    if (ipsStr) {
+        let ips = JSON.parse(ipsStr);
+        const nowSecond = parseInt(Date.parse(new Date()) / 1000);
+
+        for (let i in ips) {
+            if (ips[i].ttl >= nowSecond) {
+                return ips[i].ip;
+            }
+        }
+    }
+
+    if (!once) {
+        updateDnsCache(region, serviceName);
+        return resolveName(region, serviceName, true)
+    }
+
+    return null;
+}
