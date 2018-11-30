950d9719b80aa4fc4e45e8bd8d1077297b31ba47
luoxiaojun <luoxiaojun@dodoca.com>
2018-11-30 10:09:48 +0800

    update js

--- js/http_dns.js
+++ js/http_dns.js
@@ -1,23 +1,34 @@
-function updateDnsCache(region, serviceName) {
-    fetch('http://localhost:57267/ips?region=' + region + '&service-name=' + serviceName, {"METHOD": "GET"}).then(function (response) {
+function HttpDns (gateway) {
+    this.gateway = gateway;
+}
+
+HttpDns.prototype.now = function() {
+    return parseInt(Date.parse(new Date()) / 1000);
+};
+
+HttpDns.prototype.updateDnsCache = function(region, serviceName) {
+    let httpDns = this;
+    fetch(httpDns.gateway + '/ips?region=' + region + '&service-name=' + serviceName, {"METHOD": "GET"}).then(function (response) {
         response.json().then(function (data) {
             if (data.code === 0) {
                 let ips = data.data.ips;
+                const nowSecond = httpDns.now();
+
                 ips.forEach(function (ip, i) {
-                    ips[i].ttl = parseInt(Date.parse(new Date()) / 1000) + parseInt(ip.ttl)
+                    ips[i].ttl = nowSecond + parseInt(ip.ttl)
                 });
 
                 localStorage.setItem('dns:' + region + ':' + serviceName, JSON.stringify(ips));
             }
         });
     });
-}
+};
 
-function resolveName(region, serviceName, once = false) {
+HttpDns.prototype.resolveName = function(region, serviceName, once = false) {
     const ipsStr = localStorage.getItem('dns:' + region + ':' + serviceName);
     if (ipsStr) {
         let ips = JSON.parse(ipsStr);
-        const nowSecond = parseInt(Date.parse(new Date()) / 1000);
+        const nowSecond = this.now();
 
         for (let i in ips) {
             if (ips[i].ttl >= nowSecond) {
@@ -27,9 +38,12 @@ function resolveName(region, serviceName, once = false) {
     }
 
     if (!once) {
-        updateDnsCache(region, serviceName);
-        return resolveName(region, serviceName, true)
+        self.updateDnsCache(region, serviceName);
+        return this.resolveName(region, serviceName, true)
     }
 
     return null;
-}
+};
+
+// Demo
+// let httpDnsIns = new HttpDns('http://localhost:53103');
