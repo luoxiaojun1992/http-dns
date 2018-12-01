664d3f688ca31cc38b1da0e47947015f34ad1e89
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-01 22:47:02 +0800

    update js

--- js/http_dns.js
+++ js/http_dns.js
@@ -10,7 +10,7 @@ HttpDns.prototype.storageKey = function(region, serviceName) {
     return 'http-dns:' + region + ':' + serviceName;
 };
 
-HttpDns.prototype.updateDnsCache = function(region, serviceName) {
+HttpDns.prototype.updateDnsCache = function(region, serviceName, callback) {
     let httpDns = this;
     fetch(httpDns.gateway + '/ips?region=' + region + '&service-name=' + serviceName, {"METHOD": "GET"}).then(function (response) {
         response.json().then(function (data) {
@@ -23,12 +23,14 @@ HttpDns.prototype.updateDnsCache = function(region, serviceName) {
                 });
 
                 localStorage.setItem(httpDns.storageKey(region, serviceName), JSON.stringify(ips));
+
+                callback();
             }
         });
     });
 };
 
-HttpDns.prototype.resolveName = function(region, serviceName, once = false) {
+HttpDns.prototype.resolveName = function(region, serviceName, callback, once = false) {
     const ipsStr = localStorage.getItem(this.storageKey(region, serviceName));
     if (ipsStr) {
         let ips = JSON.parse(ipsStr);
@@ -36,18 +38,32 @@ HttpDns.prototype.resolveName = function(region, serviceName, once = false) {
 
         for (let i in ips) {
             if (ips[i].ttl >= nowSecond) {
-                return ips[i].ip;
+                callback(ips[i].ip);
+                return;
             }
         }
     }
 
     if (!once) {
-        this.updateDnsCache(region, serviceName);
-        return this.resolveName(region, serviceName, true)
+        let httpDns = this;
+        this.updateDnsCache(region, serviceName, function () {
+            httpDns.resolveName(region, serviceName, callback, true)
+        });
     }
+};
 
-    return null;
+HttpDns.prototype.fetch = function (url, options, callback) {
+    let urlObj = new URL(url);
+    this.resolveName("sh", urlObj.hostname, function (ip) {
+        if (typeof options.Headers == 'undefined') {
+            options.Headers = {"Host": urlObj.hostname};
+        } else {
+            options.Headers.Host = urlObj.hostname;
+        }
+        const newUrl = urlObj.protocol + "//" + ip + (urlObj.port.length > 0 ? ':' + urlObj.port : "") + urlObj.pathname + urlObj.search + urlObj.hash;
+        callback(fetch(newUrl, options));
+    });
 };
 
 // Demo
-// let httpDnsIns = new HttpDns('http://localhost:63186');
+// let httpDnsIns = new HttpDns('http://localhost:61596');
